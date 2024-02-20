98b9717a45e1174974ecf436d3de4d018094ab0b
Erin <erin.song@gsa.gov>
2024-02-20 14:46:42 -0800

    Test script using GitHub secrets

--- scripts/download_zone_file.sh
+++ scripts/download_zone_file.sh
@@ -1,3 +1,6 @@
 """Authenticates into ICANN using Github Actions secrets credentials and downloads .gov zone file"""
-token=$(curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"username": "erin.song@gsa.gov","password": "zone-file-test-N0w"}' https://account-api.icann.org/api/authenticate | jq -r .accessToken)
+ICANN_username=$1
+ICANN_password=$2
+authentication_data="{\"username\":\"$ICANN_username\",\"password\":\"$ICANN_password\"}"
+token=$(curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d $authentication_data https://account-api.icann.org/api/authenticate | jq -r .accessToken)
 curl -X GET --output gov.zone.txt.gz -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $token" https://czds-api.icann.org/czds/downloads/gov.zone
