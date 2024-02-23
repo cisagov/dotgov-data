1bb9c91da4946ec0a6c4db32fef4580758357d10
Erin <erin.song@gsa.gov>
2024-02-23 12:32:06 -0800

    Add tail command to unzip

--- scripts/download_zone_file.sh
+++ scripts/download_zone_file.sh
@@ -4,4 +4,4 @@ ICANN_password=$2
 authentication_data="{\"username\":\"$ICANN_username\",\"password\":\"$ICANN_password\"}"
 token=$(curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d $authentication_data https://account-api.icann.org/api/authenticate | jq -r .accessToken)
 curl -X GET --output gov.txt.gz -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $token" https://czds-api.icann.org/czds/downloads/gov.zone
-gunzip -f ./gov.txt.gz
+gunzip -f ./gov.txt.gz | tail
