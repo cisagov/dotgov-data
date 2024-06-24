c3a523c1e292768de3ec27632e54d514c18f4772
Erin <121973038+erinysong@users.noreply.github.com>
2024-06-24 13:33:32 -0700

    Export cloudflare zone file data into gov.txt

--- scripts/download_zone_file.sh
+++ scripts/download_zone_file.sh
@@ -1,6 +1,2 @@
 """Authenticates into ICANN using Github Actions secrets credentials and downloads .gov zone file"""
-ICANN_username=$1
-ICANN_password=$2
-authentication_data="{\"username\":\"$ICANN_username\",\"password\":\"$ICANN_password\"}"
-token=$(curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d $authentication_data https://account-api.icann.org/api/authenticate | jq -r .accessToken)
-[ ! -z "$token" ] && curl -X GET --output gov.txt.gz -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $token" https://czds-api.icann.org/czds/downloads/gov.zone && gunzip -f ./gov.txt.gz > gov.txt
+curl --request GET --url https://api.cloudflare.com/client/v4/zones/86b4010ebf86cb587d189fb14c565280/dns_records/export --header 'Content-Type: application/json' --header 'Authorization: Bearer Li74eWOsCC-66_zzGhbfxX4ICTZ99mLFhI5mAmS3' > gov.txt
