f7a929cbce7a24042f3d753c16b224ccebddbb24
Cameron Dixon <cameron.dixon@cisa.dhs.gov>
2026-06-10 09:32:42 -0400

    Modify download_zone_file.sh to clean output
    Add sed command to remove cf_tags from the output file.

--- scripts/download_zone_file.sh
+++ scripts/download_zone_file.sh
@@ -1,3 +1,4 @@
 zone=$1
 token=$2
 curl --request GET --url https://api.cloudflare.com/client/v4/zones/$zone/dns_records/export --header 'Content-Type: application/json' --header "Authorization: Bearer $token"
+sed -i 's/[[:blank:]]*; cf_tags=cf-proxied:false//g' gov.txt
